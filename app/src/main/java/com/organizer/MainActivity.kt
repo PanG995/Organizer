203f9a7853dbe39fc6e6f2a87e41c6759bf59462
root <dominik.golik1995@gmail.com>
2022-10-22 12:13:08 +0200

    OR04: Add dagger and hilt

--- app/src/main/java/com/organizer/MainActivity.kt
+++ app/src/main/java/com/organizer/MainActivity.kt
@@ -2,9 +2,12 @@ package com.organizer
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 }
+
